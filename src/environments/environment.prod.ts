fe1c497df89e115e1a9becae4a80345507ecc68b
SAURAVBORAH22 <sauravborah1999@gmail.com>
2025-04-03 18:48:30 +0530

    added production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
     measurementId: "G-VC3QWDBR7Y"
   },
   sellerAppUrl: 'http://localhost:4201',
-  clientAppUrl: 'http://localhost:4200'
+  clientAppUrl: 'https://swifthost-saurav-borahs-projects.vercel.app'
 };
